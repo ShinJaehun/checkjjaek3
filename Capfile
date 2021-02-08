b638966f28a5f8dddc2a3afd8794aeccbf49586c
ShinJaehun <gunsmoke.shin@gmail.com>
2021-02-08 16:59:13 +0900

    fix for deploy

--- Capfile
+++ Capfile
@@ -40,5 +40,5 @@ require 'capistrano/database_yml'
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.7.0'
+#set :rbenv_type, :user
+#set :rbenv_ruby, '2.7.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_via, :remote_cache
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/ubuntu/#{fetch :application}"
+# set :deploy_to, "/home/ubuntu/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
