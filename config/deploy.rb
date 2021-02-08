4e4bff9e9bf7596a755f02bcde654fd88abf5a67
ShinJaehun <gunsmoke.shin@gmail.com>
2021-02-08 14:14:28 +0900

    webpacker install for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,14 +60,14 @@ namespace :deploy do
   end
 end
 
-before "deploy:assets:precompile", "deploy:yarn_install"
-namespace :deploy do
-  desc "Run rake yarn install"
-  task :yarn_install do
-    on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
-      end
-    end
-  end
-end
+#before "deploy:assets:precompile", "deploy:yarn_install"
+#namespace :deploy do
+#  desc "Run rake yarn install"
+#  task :yarn_install do
+#    on roles(:web) do
+#      within release_path do
+#        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+#      end
+#    end
+#  end
+#end
