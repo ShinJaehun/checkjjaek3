988548cd34168cb2286676e29e613ba919231f77
ShinJaehun <gunsmoke.shin@gmail.com>
2021-02-08 14:09:11 +0900

    fix gitignore and deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,3 +59,15 @@ namespace :deploy do
     end
   end
 end
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
