43b586242eeb215551f037cfd7448c82321fcc7a
ShinJaehun <gunsmoke.shin@gmail.com>
2022-01-11 17:33:14 +0900

    fix post messages to other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,14 @@ class UsersController < ApplicationController
 
     def show
       # user show에서 해당 사용자에게 남긴 메시지를 확인할 수 있음.
-      @receive_messages = Message.where(receiver_id: @user.id)
-      @posts = @user.posts.where.not(postable_type: 'Message').or(Post.where(postable: @receive_messages)).order(created_at: :desc)
+      #@receive_messages = Message.where(receiver_id: @user.id)
+      #@posts = @user.posts.where.not(postable_type: 'Message')
+      #  .or(Post.where(postable: @receive_messages)).order(created_at: :desc)
+
+      @posts = @user.posts.where.not(postable_type: "Message")
+        .or(Post.where(postable_type: "Message")
+        .where(id: PostRecipientUser.where(recipient_user_id: @user.id).pluck(:post_id)))
+        .order(created_at: :desc)
 
       # 다른 사용자에게 message 남기기
       @message = Message.new
