395651a8927bbf34fc6c7e2b8d8a2f5607d84123
ShinJaehun <gunsmoke.shin@gmail.com>
2022-01-23 17:04:31 +0900

    fix information partial of groups view

--- config/routes.rb
+++ config/routes.rb
@@ -35,10 +35,10 @@ Rails.application.routes.draw do
 
   get 'search', to: 'posts#search'
 
-  post 'join_group/:id', to: 'groups#join_group', as: :join_group
-  post 'apply_group/:id', to: 'groups#apply_group', as: :apply_group
-  post 'approve_user/:id', to: 'groups#approve_user', as: :approve_user
-  post 'suspend_user/:id', to: 'groups#suspend_user', as: :suspend_user
+  get 'join_group/:id', to: 'groups#join_group', as: :join_group
+  get 'apply_group/:id', to: 'groups#apply_group', as: :apply_group
+  get 'approve_user/:id', to: 'groups#approve_user', as: :approve_user
+  get 'suspend_user/:id', to: 'groups#suspend_user', as: :suspend_user
   delete 'leave_group/:id', to: 'groups#leave_group', as: :leave_group
   delete 'cancel_apply_group/:id', to: 'groups#cancel_apply_group', as: :cancel_apply_group
 
